cb987b039b57385fe805098096a3a118bee85b51
Dauthlin <70644516+Dauthlin@users.noreply.github.com>
2023-02-27 16:39:02 +0000

    sequence count bash done

--- FASTQ_Sequence_Count.sh
+++ FASTQ_Sequence_Count.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-file="Testing/1_control_ITS2_2019_minq7.fastq"
-file2="Testing/fake_test_file"
-grep -cvP '^\s*$' $file
 
-read -p "Press any key to continue "
+# debugging test files
+#file="Testing/1_control_ITS2_2019_minq7.fastq"
+#file2="Testing/fake_test_file"
+
+# total stores the number of lines that do not start with 0 or more white spaces then end
+total=$(grep -cvP '^\s*$' $1)
+echo $((total / 4))
+#read -p "Press any key to continue "
