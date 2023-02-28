cfeb518e3b5b1d093b4573463b72ad37b809c0f2
Dauthlin <70644516+Dauthlin@users.noreply.github.com>
2023-02-28 10:27:05 +0000

    added tests for program

--- Testing/test_nucleotide_count.py
+++ Testing/test_nucleotide_count.py
@@ -1,2 +1,46 @@
-def test_initialize():
-    assert True == True
+import os
+import subprocess
+
+
+def run(cmd):
+    # print(cmd)
+    try:
+        completed = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+
+    return completed
+
+
+def test_file_and_nucleotide():
+    # gets the path of the program to test
+    program_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "FASTQ_Nucleotides_Count.py")
+    # gets the path of the data
+    data_path = os.path.join(os.path.dirname(__file__), "fake_test_file.fastq")
+    # combines them into a single command
+    command = str("python " + program_path + " -f " + data_path + " -n true")
+    # runs the command
+    result = (run(cmd=command)).decode('ascii').strip()
+    assert str(result) == "60"
+
+
+def test_file_and_series_and_nucleotide():
+    # gets the path of the program to test
+    program_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "FASTQ_Nucleotides_Count.py")
+    # gets the path of the data
+    data_path = os.path.join(os.path.dirname(__file__), "fake_test_file.fastq")
+    # combines them into a single command
+    command = str("python " + program_path + " -f " + data_path + " -n true -s true")
+    # runs the command
+    result = (run(cmd=command)).decode('ascii').strip()
+    assert str(result) == '60\r\n0.0'
+
+
+if __name__ == '__main__':
+    # debugging area
+    program_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "FASTQ_Nucleotides_Count.py")
+    data_path = os.path.join(os.path.dirname(__file__), "fake_test_file.fastq")
+    command = str("python " + program_path + " -f " + data_path + " -s true")
+    result = (run(cmd=command)).decode('ascii').strip()
+    #
+    print(result)
--- Testing/test_sequence_count.py
+++ Testing/test_sequence_count.py
@@ -1,11 +1,6 @@
 import os
 import subprocess
 
-
-def test_initialize():
-    assert True == False
-
-
 def run(cmd):
     # print(cmd)
     try:
@@ -16,7 +11,7 @@ def run(cmd):
     return completed
 
 
-def test_file_and_series():
+def test_file_and_series_with_blanks():
     # gets the path of the program to test
     program_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "FASTQ_Sequence_Count.py")
     # gets the path of the data
@@ -25,10 +20,22 @@ def test_file_and_series():
     command = str("python " + program_path + " -f " + data_path + " -s true")
     # runs the command
     result = (run(cmd=command)).decode('ascii').strip()
-    assert str(result) == "1.75"
+    assert result == "1.75"
+
+def test_file_and_series_no_blanks():
+    # gets the path of the program to test
+    program_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "FASTQ_Sequence_Count.py")
+    # gets the path of the data
+    data_path = os.path.join(os.path.dirname(__file__), "fake_test_file.fastq")
+    # combines them into a single command
+    command = str("python " + program_path + " -f " + data_path + " -s true")
+    # runs the command
+    result = (run(cmd=command)).decode('ascii').strip()
+    assert result == "0.0"
 
 
 if __name__ == '__main__':
+    # debugging area
     program_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "FASTQ_Sequence_Count.py")
     data_path = os.path.join(os.path.dirname(__file__), "fake_test_file_with_blank_lines.fastq")
     command = str("python " + program_path + " -f " + data_path + " -s true")
