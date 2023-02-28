b4c0565e73c959cf7d935b7ba09bfa89271e2fb6
Dauthlin <70644516+Dauthlin@users.noreply.github.com>
2023-02-28 10:16:14 +0000

    remade test to work on shell

--- Testing/test_sequence_count.py
+++ Testing/test_sequence_count.py
@@ -1,25 +1,37 @@
 import os
-from subprocess import check_output
-
-def test_initialize():
-    assert True == True
+import subprocess
 
 
-# this block works but only on windows
-# def run(cmd):
-#     completed = check_output(["powershell", "-Command", cmd])
-#     return completed
+# def test_initialize():
+#     assert True == True
 #
-#
-# def test_file_and_series():
-#     # gets the path of the program to test
-#     data_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "FASTQ_Sequence_Count.py")
-#     # runs the program on the copmmand line
-#     result = (run(cmd="python " + data_path + " -f fake_test_file_with_blank_lines.fastq -s true")).decode('ascii').strip()
-#     assert str(result) == "1.75"
-#
-#
-# if __name__ == '__main__':
-#     data_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "FASTQ_Sequence_Count.py")
-#     result = (run(cmd="python " + data_path + " -f fake_test_file_with_blank_lines.fastq")).decode('ascii').strip()
-#     print(result)
+
+def run(cmd):
+    # print(cmd)
+    try:
+        completed = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+
+    return completed
+
+
+def test_file_and_series():
+    # gets the path of the program to test
+    program_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "FASTQ_Sequence_Count.py")
+    # gets the path of the data
+    data_path = os.path.join(os.path.dirname(__file__), "fake_test_file_with_blank_lines.fastq")
+    # combines them into a single command
+    command = str("python " + program_path + " -f " + data_path + " -s true")
+    # runs the command
+    result = (run(cmd=command)).decode('ascii').strip()
+    assert str(result) == "1.75"
+
+
+if __name__ == '__main__':
+    program_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "FASTQ_Sequence_Count.py")
+    data_path = os.path.join(os.path.dirname(__file__), "fake_test_file_with_blank_lines.fastq")
+    command = str("python " + program_path + " -f " + data_path + " -s true")
+    result = (run(cmd=command)).decode('ascii').strip()
+    #
+    print(result)
