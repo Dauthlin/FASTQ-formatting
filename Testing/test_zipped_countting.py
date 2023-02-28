17fc8d6dfadbdc5d99b410f2759b1cedc278908d
Dauthlin <70644516+Dauthlin@users.noreply.github.com>
2023-02-28 10:41:16 +0000

    updating tests for unix

--- Testing/test_zipped_countting.py
+++ Testing/test_zipped_countting.py
@@ -56,7 +56,7 @@ def test_file_and_series_and_nucleotide_zipped():
     command = str("python " + program_path + " -f " + data_path + " -n true -s true")
     # runs the command
     result = (run(cmd=command)).decode('ascii').strip()
-    assert str(result) == '60\r\n0.0'
+    assert str(result) == '60\n0.0'
 
 
 
